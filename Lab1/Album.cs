e7a94afef8ba84915dbd4189a7572ff8f003e558
ManonBiere-STUDENT <ManonBiere-STUDENT@S-3V1H504-SL09>
2026-01-22 12:19:06 +0000

    Lab 1 Updated with what was already done

---
+++ Lab1/Album.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_1_Sheet_1
+{
+    public class Album
+    {
+        //Proprietes
+        public string Name { get; set; }
+        public int YearOfRelease { get; set; }
+        public int Sales { get; set; }
+
+        //Constructor
+        public Album(string name, int yearOfRelease, int sales)
+        {
+            Name = name;
+            YearOfRelease = yearOfRelease;
+            Sales = sales;
+        }
+        public Album() { }
+
+        //Methods
+        public override string ToString()
+        {
+            return string.Format($"{Name} {YearOfRelease} {Sales}");
+        }
+
+
+    }
+}
--- Lab1/MainWindow.xaml.cs
+++ Lab1/MainWindow.xaml.cs
@@ -13,16 +13,66 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Lab1
+namespace Lab_1_Sheet_1
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Band> allBands = new List<Band>();
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            RockBand b1 = new RockBand() { BandName = "The Foo Fighters", YearFormed = 1994, Members = "Dave Grohl, Nate Mendel, Pat Smear, Taylor Hawkins, Chris Shifflett, Remi Jafee" };
+            RockBand b2 = new RockBand() { BandName = "The Rolling Stones", YearFormed = 1962, Members = "Mick Jagger, Ian Stewart, Dick Taylor, Bill Wyman, Mick Taylor" };
+
+            PopBand b3 = new PopBand() { BandName = "The Beatles", YearFormed = 1960, Members = "John Lennon Paul McCartney, George Harrison, Ringo Starr" };
+            PopBand b4 = new PopBand() { BandName = "The Green Day", YearFormed = 1986, Members = "Billie Joe Armstrong, Mike Dirnt, Tre Cool" };
+
+            IndieBand b5 = new IndieBand() { BandName = "Artic Monkeys", YearFormed = 2002, Members = "Alex Turner, Matt Heldens, Jamie Cook, Nick O'Malley" };
+            IndieBand b6 = new IndieBand() { BandName = "The Strokes", YearFormed = 1998, Members = "Julian Casablancas, Nick Valensi, Albert Hammond Jr, Nikolai Fraiture, Fabrizio Moretti" };
+
+            //Create albums
+            Random rand = new Random();
+
+            Album a1 = new Album() { Name = "Greatest Hits", YearOfRelease = rand.Next(1960, 2020), Sales = rand.Next(1000000, 1000000) };
+            Album a2 = new Album() { Name = "One by one", YearOfRelease = rand.Next(1960, 2020), Sales = rand.Next(1000000, 1000000) };
+
+            Album a3 = new Album() { Name = "Greatest Hits", YearOfRelease = rand.Next(1960, 2020), Sales = rand.Next(1000000, 1000000) };
+            Album a4 = new Album() { Name = "Greatest Hits", YearOfRelease = rand.Next(1960, 2020), Sales = rand.Next(1000000, 1000000) };
+
+            Album a5 = new Album() { Name = "Greatest Hits", YearOfRelease = rand.Next(1960, 2020), Sales = rand.Next(1000000, 1000000) };
+            Album a6 = new Album() { Name = "Greatest Hits", YearOfRelease = rand.Next(1960, 2020), Sales = rand.Next(1000000, 1000000) };
+
+            Album a7 = new Album() { Name = "Greatest Hits", YearOfRelease = rand.Next(1960, 2020), Sales = rand.Next(1000000, 1000000) };
+            Album a8 = new Album() { Name = "Greatest Hits", YearOfRelease = rand.Next(1960, 2020), Sales = rand.Next(1000000, 1000000) };
+
+            Album a9 = new Album() { Name = "Greatest Hits", YearOfRelease = rand.Next(1960, 2020), Sales = rand.Next(1000000, 1000000) };
+            Album a10 = new Album() { Name = "Greatest Hits", YearOfRelease = rand.Next(1960, 2020), Sales = rand.Next(1000000, 1000000) };
+
+            Album a11 = new Album() { Name = "Greatest Hits", YearOfRelease = rand.Next(1960, 2020), Sales = rand.Next(1000000, 1000000) };
+            Album a12 = new Album() { Name = "Greatest Hits", YearOfRelease = rand.Next(1960, 2020), Sales = rand.Next(1000000, 1000000) };
+
+            //Add to collection
+            allBands.Add(b1);
+            allBands.Add(b2);
+            allBands.Add(b3);
+            allBands.Add(b4);
+            allBands.Add(b5);
+            allBands.Add(b6);
+
+            //Sort bands
+            allBands.Sort();
+
+            //Display
+            lbxBands.ItemsSource = allBands;
+
+
         }
     }
 }
