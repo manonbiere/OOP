fc83bdf1d932ecf95ed76c89804d9f42f110c335
Manon Biere <S00295652@atu.ie>
2026-01-28 10:27:12 +0000

    ToQuestion5

---
+++ Lab2/MyFileInfo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lab2
+{
+    public class MyFileInfo
+    {
+        public string Name { get; set; }
+        public long Length { get; set; }
+        public DateTime CreationTime { get; set; }
+
+        /*public MyFileInfo(string name, long length, DateTime creationTime)
+        {
+            Name = name;
+            Length = length;
+            CreationTime = creationTime;
+        }*/
+
+        public override string ToString()
+        {
+            return string.Format("{0,-40}{1,6:F0} MB {2}", Name, Length / 1000, CreationTime.ToShortDateString());
+        }
+    }
+}
---
+++ Lab2/Program.cs
@@ -0,0 +1,120 @@
+﻿using Lab2;
+using System;
+using System.Linq;
+using System.Xml.Linq;
+using System.IO;
+
+//EXERCICE 1
+/*class Program
+{
+    static void Main()
+    {
+        int[] numbers = { 1, 5, 3, 6, 11, 2, 15, 21, 13, 12, 10 };
+
+        var outputNumbers = from num in numbers
+                            where num > 5
+                            orderby num
+                            select num;
+
+        foreach (int num in outputNumbers)
+        {
+            Console.WriteLine(num.ToString());
+        }
+
+        Console.ReadLine();
+    }
+}*/
+
+//EXERCICE 2
+/*class Program
+{
+    static void Main()
+    {
+        int[] numbers = { 1, 5, 3, 6, 11, 2, 15, 21, 13, 12, 10 };
+
+        var outputNumbers = numbers.Where(n => n > 5).OrderByDescending(n => n);
+
+        foreach (int num in outputNumbers)
+        {
+            Console.WriteLine(num);
+        }
+
+        Console.ReadLine();
+    }
+}*/
+
+//EXERCICE 3
+/*class Program
+{
+    static void Main()
+    {
+        FileInfo[] files = new DirectoryInfo(@"c:\windows").GetFiles();
+
+        var query = from item in files
+                    where item.Length > 10000
+                    orderby item.Length descending, item.Name
+                    select new MyFileInfo
+                    {
+                        Name = item.Name,
+                        Length = item.Length,
+                        CreationTime = item.CreationTime,
+                    };
+        Console.WriteLine("{0,-40}{1,6} MB {2}", "FileName", "Size", "Creation Date");
+
+        foreach (MyFileInfo item in query)
+        {
+            Console.WriteLine(item);
+        }
+
+        Console.ReadLine();
+    }
+}*/
+
+//EXERCICE 4
+/*class Program
+{
+    static void Main()
+    {
+        var files = new DirectoryInfo(@"c:\windows").GetFiles();
+
+        var query = from item in files
+                    where item.Length > 10000
+                    orderby item.Length descending, item.Name
+                    select new
+                    {
+                        Name = item.Name,
+                        Length = item.Length,
+                        CreationTime = item.CreationTime,
+                    };
+        Console.WriteLine("FileName\t\tSize\t\tCreation Date");
+
+        foreach (var item in query)
+        {
+            Console.WriteLine("{0} \t\t{1} bytes,\t{2}", item.Name, item.Length, item.CreationTime);
+        }
+
+        Console.ReadLine();
+    }
+}*/
+
+//EXERCICE 5
+class Program
+{
+    static void Main()
+    {
+        var files = new DirectoryInfo(@"c:\windows").GetFiles();
+
+        var query = files.Where(f => f.Length > 10000)
+            .OrderBy(files => files.Length).ThenBy(f => f.Name)
+            .Select(f =>new {Name = f.Name, Length = f.Length,CreationTime = f.CreationTime });
+        
+        Console.WriteLine("FileName\t\tSize\t\tCreation Date");
+
+        foreach (var item in query)
+        {
+            Console.WriteLine("{0} \t\t{1} bytes,\t{2}", item.Name, item.Length, item.CreationTime);
+        }
+
+        Console.ReadLine();
+    }
+}
