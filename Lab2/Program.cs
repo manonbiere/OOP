73fcef885246494966e3935dea21260256ce323d
Manon Biere <S00295652@atu.ie>
2026-01-28 11:07:22 +0000

    End of TP2

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Xml.Linq;
 using System.IO;
 
-//EXERCICE 1
+//EXERCISE 1
 /*class Program
 {
     static void Main()
@@ -25,7 +25,7 @@ using System.IO;
     }
 }*/
 
-//EXERCICE 2
+//EXERCISE 2
 /*class Program
 {
     static void Main()
@@ -43,7 +43,7 @@ using System.IO;
     }
 }*/
 
-//EXERCICE 3
+//EXERCISE 3
 /*class Program
 {
     static void Main()
@@ -70,7 +70,7 @@ using System.IO;
     }
 }*/
 
-//EXERCICE 4
+//EXERCISE 4
 /*class Program
 {
     static void Main()
@@ -97,8 +97,8 @@ using System.IO;
     }
 }*/
 
-//EXERCICE 5
-class Program
+//EXERCISE 5
+/*class Program
 {
     static void Main()
     {
@@ -113,6 +113,167 @@ class Program
         foreach (var item in query)
         {
             Console.WriteLine("{0} \t\t{1} bytes,\t{2}", item.Name, item.Length, item.CreationTime);
+        }
+
+        Console.ReadLine();
+    }
+}*/
+
+//EXERCISE 6
+/*class Program
+{
+    public static int DoubleIt(int value)
+    {
+        Console.WriteLine("About to double Number " + value.ToString());
+        return value * 2;
+    }
+
+    static void Main(string[] args)
+    {
+        int[] numbers = { 1, 5, 3, 6, 10, 12, 8 };
+
+        //var query = from number in numbers
+        //            select DoubleIt(number);
+
+        var query = numbers.Select(n => DoubleIt(n));
+
+        //var query2 = from n in numbers
+        //             select(DoubleIt(n));
+
+        Console.WriteLine("Before the foreach loop");
+
+        foreach(var item in query)
+        {
+            Console.WriteLine(item);
+        }
+
+        Console.ReadLine();
+    }
+}*/
+
+//EXERCISE 7
+/*class Program
+{
+    public static int DoubleIt(int value)
+    {
+        Console.WriteLine("About to double Number " + value.ToString());
+        return value * 2;
+    }
+
+    static void Main(string[] args)
+    {
+        int[] numbers = { 1, 5, 3, 6, 10, 12, 8 };
+
+        var query1 = from number in numbers
+                     orderby number descending
+                     select number;
+
+        var query2 = from number in query1
+                     where number < 8
+                     select number;
+
+        var query3 = from number in query2
+                     select DoubleIt(number);
+
+        foreach (var item in query3)
+        {
+            Console.WriteLine(item);
+        }
+
+        Console.ReadLine();
+    }
+}*/
+
+//EXERCISE 8 9 and 10
+/*class Program
+{
+    static void Main(string[] args)
+    {
+        string[] n = { "Mary", "Joseph", "Michael", "Sarah", "Margaret", "John"};
+
+        //Alphabetical order
+        var query1 = from name in n
+                     orderby name descending
+                     select name;
+
+        //Name longer than four letters
+        var query2 = from name in n
+                     where name.Length > 4
+                     select name;
+        //Only begins with M
+        var query3 = from name in n
+                     where name[0] == 'M'
+                     select name;
+
+        Console.WriteLine("Exercise 8 :");
+        foreach (var item in query2)
+        {
+            Console.WriteLine("\t" + item);
+        }
+
+        Console.WriteLine("\nExercise 9 :");
+        foreach (var item in query1)
+        {
+            Console.WriteLine("\t" + item);
+        }
+
+        Console.WriteLine("\nExercise 10 :");
+        foreach (var item in query3)
+        {
+            Console.WriteLine("\t" + item);
+        }
+
+        Console.ReadLine();
+    }
+}
+*/
+
+//Exercise 11 and 12
+class Program
+{
+    public class Customer
+    {
+        public string Name;
+        public string City;
+    }
+
+    private static List<Customer> GetCustomers()
+    {
+        Customer c1 = new Customer { Name = "Tom", City = "Dublin" };
+        Customer c2 = new Customer { Name = "Sally", City = "Galway" };
+        Customer c3 = new Customer { Name = "George", City = "Cork" };
+        Customer c4 = new Customer { Name = "Molly", City = "Dublin" };
+        Customer c5 = new Customer { Name = "Joe", City = "Galway" };
+        List<Customer> customers = new List<Customer>();
+        customers.Add(c1);
+        customers.Add(c2);
+        customers.Add(c3);
+        customers.Add(c4);
+        customers.Add(c5);
+        return customers;
+    }
+    static void Main(string[] args)
+    {
+        //Customers from Dublin
+        var query1 = from customer in GetCustomers()
+                     where customer.City == "Dublin"
+                     select customer;
+
+        //Ordering Customers by City and by Name
+        var query2 = from customer in GetCustomers()
+                     orderby customer.City, customer.Name descending
+                     select customer;
+
+        Console.WriteLine("Exercise 10 :");
+        foreach (var item in query1)
+        {
+            Console.WriteLine("\tName : " + item.Name + "\tCity: " + item.City);
+        }
+
+        Console.WriteLine("\nExercise 11 :");
+        foreach (var item in query2)
+        {
+            Console.WriteLine("\tName : " + item.Name + "\tCity: " + item.City);
         }
 
         Console.ReadLine();
