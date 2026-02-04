53bd1e6bd59cb39aa1b44127bf5a15549dd31e6c
Manon Biere <S00295652@atu.ie>
2026-02-04 10:00:55 +0000

    first commit project

--- PersonalProject/MainWindow.xaml.cs
+++ PersonalProject/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace PersonalProject
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
