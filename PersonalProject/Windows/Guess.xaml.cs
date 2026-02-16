4e27361cf6c61117675b35cb6e89d9f78a079167
Manon Biere <S00295652@atu.ie>
2026-02-16 11:50:54 +0000

    advance

--- PersonalProject/Windows/Guess.xaml.cs
+++ PersonalProject/Windows/Guess.xaml.cs
@@ -20,6 +20,11 @@ namespace PersonalProject.Windows
     /// </summary>
     public partial class Guess : Window
     {
+        public Pokemon toGuess;
+        public int nbClues;
+        public int streak;
+        public int totalWin;
+        public List<int> clues = new List<int>();
         public Guess()
         {
             InitializeComponent();
@@ -54,6 +59,55 @@ namespace PersonalProject.Windows
             Pokedex1 next = new Pokedex1();
             next.Show();
             this.Close();
+        }
+
+        public void RandomClue(int num)
+        {
+            Random rand = new Random();
+            int random = rand.Next(1, num);
+            foreach (int i in clues) 
+            { 
+                if (i == random)
+                {
+                    random = rand.Next(1, num);
+                }
+            }
+        }
+
+        public void NextClue(int clue)
+        {
+            if (clue == 0)
+            {
+                lblType.Content = "Type : " + toGuess.Type;
+            }
+            if (clue == 1)
+            {
+                lblGeneration.Content = "Generation :" + toGuess.Generation;
+            }
+            if (clue == 2)
+            {
+                lblCategory.Content = "Category : " + toGuess.Category;
+            }
+            if (clue == 3)
+            {
+                lblEvolution.Content = "Evolution :" + toGuess.Evolution;
+            }
+            else
+            {                
+                if (clue == 4)
+                {
+                    RandomClue(5);
+                }
+                if (clue == 5)
+                {
+                    lblClue2.Content = "Clue 2 : " + toGuess.Category;
+                }
+                if (clue == 6)
+                {
+                    lblClue3.Content = "Clue 3 :" + toGuess.Evolution;
+                }
+            }
+            
         }
     }
 }
